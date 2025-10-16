bc164dcd6e3dea4fe5ee59990fae49c8b185bf24
olusola <somorinolusola1@gmail.com>
2025-10-16 02:15:30 +0100

    Day 1 second exexercise

---
+++ Day_1/profile-component/README.md
@@ -0,0 +1,20 @@
+# 🎯 Practice Exercise 2: Profile Component
+
+**Task:** Build a simple profile card using JSX
+
+**Requirements:**
+1. Create `Profile.jsx`
+2. Inside the component, add:
+   * An image tag (use any placeholder URL)
+   * Your name in `<h2>`
+   * Your role (e.g., "Student Developer") in `<h3>`
+   * One sentence about yourself in `<p>`
+3. Use this component in `App.jsx`
+
+**Sample Output:**
+
+## Desktop view
+![Profile](<Screenshot 2025-10-16 020545.png>)
+
+## Mobile view
+![Profile](<Screenshot 2025-10-16 021411.png>)
---
+++ Day_1/profile-component/src/Profile.jsx
@@ -0,0 +1,21 @@
+function Profile() {
+    return (
+        <div className="container">
+            <div>
+                <img src="https://i.pinimg.com/1200x/3e/f3/50/3ef350dc86cc82a092463e5d795654b5.jpg" alt="image" />
+                <h2>Olusola Edward</h2>
+                <h3>AI Developer</h3>
+                <p>Learning React to build amazing web apps in Nigeria.</p>
+            </div>
+            <div>
+                <img src="https://i.pinimg.com/1200x/6d/96/4a/6d964a36bae56a6c3a22878b8987bd6c.jpg" alt="image" />
+                <h2>Asherkine</h2>
+                <h3>Philanthropist</h3>
+                <p>Touching life and inspiring people.</p>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Profile;
